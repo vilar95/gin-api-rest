7cf878cf6987041d089b2d1ec194bfc44fdd310a
Eduardo Vilar <eduardo.vilar@pinpag.com.br>
2026-02-19 22:02:05 -0300

    feat: adiciona testes para operações de CRUD de alunos e refatora mock de estudante

--- cmd/api/main_test.go
+++ cmd/api/main_test.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -21,7 +25,7 @@ func SetupRouteTest() *gin.Engine {
 }
 
 func CreateStudentMock() {
-	student := model.Student{Name: "Nome do aluno teste", RG: "123456789", CPF: "12345678901"}
+	student := model.Student{Name: "Jarbinhas a IA do Bem", RG: "123456789", CPF: "12345678901"}
 	database.DB.Create(&student)
 	ID = int(student.ID)
 }
@@ -66,5 +70,55 @@ func TestFetchStudentByCPF(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/student/cpf/123.456.789-10", nil)
 	res := httptest.NewRecorder()
 	r.ServeHTTP(res, req)
+	assert.Equal(t, http.StatusOK, res.Code, "O status code esperado era 200, mas veio %d", res.Code)
+}
+
+func TestFetchStudentBy(t *testing.T) {
+	database.ConnectDatabase()
+	CreateStudentMock()
+	defer DeleteStudentMock()
+	r := SetupRouteTest()
+	r.GET("/student/:id", controller.FetchStudentByID)
+	path := "/student/" + strconv.Itoa(ID)
+	req, _ := http.NewRequest("GET", path, nil)
+	res := httptest.NewRecorder()
+	r.ServeHTTP(res, req)
+	var stutentMock model.Student
+	json.Unmarshal(res.Body.Bytes(), &stutentMock)
+	fmt.Println(stutentMock.Name)
+	assert.Equal(t, "Jarbinhas a IA do Bem", stutentMock.Name, "O nome do estudante esperado era 'Jarbinhas a IA do Bem', mas veio %s", stutentMock.Name)
+	assert.Equal(t, "123456789", stutentMock.RG, "O RG do estudante esperado era '123456789', mas veio %s", stutentMock.RG)
+	assert.Equal(t, "12345678901", stutentMock.CPF, "O CPF do estudante esperado era '12345678901', mas veio %s", stutentMock.CPF)
+}
+
+func TestDeleteStudent(t *testing.T) {
+	database.ConnectDatabase()
+	CreateStudentMock()
+	r := SetupRouteTest()
+	r.DELETE("/delete-student/:id", controller.DeleteStudent)
+	path := "/delete-student/" + strconv.Itoa(ID)
+	req, _ := http.NewRequest("DELETE", path, nil)
+	res := httptest.NewRecorder()
+	r.ServeHTTP(res, req)
+	assert.Equal(t, http.StatusOK, res.Code, "O status code esperado era 200, mas veio %d", res.Code)
+}
+
+func TestUpdateStudent(t *testing.T) {
+	database.ConnectDatabase()
+	CreateStudentMock()
+	defer DeleteStudentMock()
+	r := SetupRouteTest()
+	r.PATCH("/update-student/:id", controller.UpdateStudent)
+	path := "/update-student/" + strconv.Itoa(ID)
+	student := model.Student{Name: "Airton Marques", RG: "423456781", CPF: "52345678901"}
+	studentJSON, _ := json.Marshal(student)
+	req, _ := http.NewRequest("PATCH", path, bytes.NewBuffer(studentJSON))
+	res := httptest.NewRecorder()
+	r.ServeHTTP(res, req)
+	var updatedStudent model.Student
+	json.Unmarshal(res.Body.Bytes(), &updatedStudent)
+	assert.Equal(t, "Airton Marques", updatedStudent.Name, "O nome do estudante esperado era 'Airton Marques', mas veio %s", updatedStudent.Name)
+	assert.Equal(t, "423456781", updatedStudent.RG, "O RG do estudante esperado era '423456781', mas veio %s", updatedStudent.RG)
+	assert.Equal(t, "52345678901", updatedStudent.CPF, "O CPF do estudante esperado era '52345678901', mas veio %s", updatedStudent.CPF)
 	assert.Equal(t, http.StatusOK, res.Code, "O status code esperado era 200, mas veio %d", res.Code)
 }
